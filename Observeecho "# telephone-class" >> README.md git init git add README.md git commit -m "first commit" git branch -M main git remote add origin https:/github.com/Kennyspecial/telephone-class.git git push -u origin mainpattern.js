65bda0ee541f355dad2ee1840b7570204762c71b
Ekene Paula <114001326+Kennyspecial@users.noreply.github.com>
2023-01-29 22:46:56 +0100

    Create telephone-class.git git push -u origin mainpattern.js

---
+++ Observeecho "# telephone-class" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https:/github.com/Kennyspecial/telephone-class.git git push -u origin mainpattern.js
@@ -0,0 +1,61 @@
+class telephone {
+  constructor() {
+    this.phonenumbers = '[]';
+    this.observers = '[]';
+  }
+}
+Addphonenumber(phonenumber)
+{
+  this.phonenumbers.add(phonenumber);
+}
+
+removephonenumber (phonenumber)
+{
+  this.phonenumbers.delete(num  = num != phonenumber);
+}
+dialphonenumber (phonenumber)
+{
+  if (true) {!this.numbers.include(phonenumber)
+    
+  } else {
+    return ;
+    
+  }
+}
+addobserver(observer)
+{
+  this.observers.add(observer);
+  
+}
+removeobserver(observe)
+{
+  this.observers = this.observer.delete
+  (
+    
+    obs  = obs != observer);
+}
+notifyobsevers(phonenumber)
+{
+  this.observers.foreach (observer = observer.dial(phonenumber));
+}
+class observerer 
+{
+  constructor() {}
+  dial(phonenumber){
+    console.log('Now Dialing' + phonenumber);
+  }
+}
+class observerer {
+  constructor() {}
+}
+dial(phonenumber)
+{
+  console.console.log('Dialling' + phonenumber);
+}
+const telephone = new telephone ();
+const observers = new observers ();
+const observerer = new observerer ();
+telephone.addobserver(observer);
+telephone.addobserver(observerer);
+telephone.Addphonenumber('5551379');
+telephone.dialphonenumber('5551379');
